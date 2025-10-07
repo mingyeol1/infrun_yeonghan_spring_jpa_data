90e0b0ccba69fa8b6c47820c2290eb1815e02c1e
MinGyeol <alsruf741@naver.com>
2025-10-07 17:04:31 +0900

    동작확인

--- src/test/java/study/data_jpa/DataJpaApplicationTests.java
+++ src/test/java/study/data_jpa/DataJpaApplicationTests.java
@@ -2,8 +2,10 @@ package study.data_jpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class DataJpaApplicationTests {
 
 	@Test
--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -1,30 +1,29 @@
 package study.data_jpa.repository;
 
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-//@Rollback(value = false)
-class MemberJpaRepositoryTest {
-
+class MemberRepositoryTest {
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
+
 
     @Test
     public void testMember(){
         Member member = new Member("memberA");
-        Member saveMember = memberJpaRepository.save(member);
+        Member saveMember = memberRepository.save(member);
 
-        Member findMember = memberJpaRepository.find(saveMember.getId());
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
 
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
@@ -33,5 +32,6 @@ class MemberJpaRepositoryTest {
 
 
     }
+
 
 }
